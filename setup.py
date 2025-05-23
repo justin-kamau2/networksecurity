c22677294fea7dbe81a564a429487e318009f80f
Justin Mbugua <justinmbugua@gmail.com>
2025-05-23 19:54:47 +0300

    Setup.py file ready

--- setup.py
+++ setup.py
@@ -22,8 +22,20 @@ def get_requirements()->List[str]:
             for line in lines:
                 requirement = line.strip()
                 # ignore empty lines and -e .
-                if requirement and requirements!= '-e .':
+                if requirement and requirement!= '-e .':
                     requirement_lst.append(requirement)
     except FileNotFoundError:
         print("requirements.txt file not found")
+
+    return requirement_lst
+#print(get_requirements())  <- at this point run script in terminal to see whether you get a list of packages as output
+
+setup(
+    name = "NetworkSecurity",
+    version = "0.0.1",
+    author = "Justin Kamau",
+    author_email = "kamaujustin63@gmail.com",
+    packages = find_packages(),     #Automatically finds all Python packages in the project
+    install_requires = get_requirements()       #Installs dependencies from requirements.txt
+)
         
